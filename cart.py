eea8df12e30d71fe3a7ff414869fe21454cdac1e
dwkula <dawid.kula98@gmail.com>
2023-06-14 13:02:13 +0200

    v 0.3

---
+++ cart.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn import tree
+
+import plotly.express as px
+import plotly.graph_objects as go
+import graphviz
